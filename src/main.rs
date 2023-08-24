6563fec3d672574ad913712c45e050d8b564cecd
JThomasDevs <jthomasahs14@gmail.com>
2023-08-23 18:05:15 -0600

    English clips re-write
    Essentially a clone of just-chatting-clip-helper but only gets English clips

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,139 @@
-use playwright::api::{ElementHandle, Page};
-use playwright::Playwright;
+use std::fs;
+use playwright::*;
+use playwright::api::ElementHandle;
+use tokio;
 
 #[tokio::main]
-async fn main() -> Result<(), playwright::Error> {
+async fn main() -> Result<(), Error> {
     let playwright = Playwright::initialize().await?;
     playwright.prepare()?; // Install browsers
 
-    let webkit = playwright.chromium();
-    let browser = webkit
-        .launcher()
-        .headless(false)
+    // Browser settings
+    let webkit = playwright.webkit();
+    let browser = webkit.launcher()
+        .headless(true)
+        .launch().await?;
+
+    // Each context is a window, each page is a tab
+    let context = browser.context_builder()
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+        .build().await?;
+    let page = context.new_page().await?;
+
+    // Navigate to page
+    page.goto_builder("https://www.twitch.tv/directory/category/just-chatting/clips?featured=false&range=7d")
+        .goto().await?;
+    println!("Clips page loaded");
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+    // Change language to English
+    // open language menu
+    page.query_selector("div[class='Layout-sc-1xcs6mc-0 fFENuB']").await?.unwrap().query_selector("button").await?.expect("Couldn't open language menu").click_builder().click().await?;
+    tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+    // click English
+    page.query_selector("div[class='Layout-sc-1xcs6mc-0 gWkOOv']").await?.expect("Couldn't click on English").click_builder().click().await?;
+
+    // Find all initial clips and then click a blank part of the page so the keyboard may be used
+    let mut clips = page.query_selector_all("div[class='Layout-sc-1xcs6mc-0 iPAXTU']").await?;
+    page.query_selector("div[data-a-target='root-scroller']").await?
+        .expect("Can't find where to click :(")
+        .click_builder()
+        .click().await?;
+    while clips.len() < 80 { // Gather 80 clip container elements in a vector
+        page.keyboard
+            .down("Space").await?;
+        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+        clips = page.query_selector_all("div[class='Layout-sc-1xcs6mc-0 iPAXTU']").await?;
+    }
+    println!("Found {} clips", clips.len());
+    // Get a link for each clip
+    let (streamer_vec, url_vec) = clip_links(clips).await;
+    // Navigate to third-party website to download clips
+    // Close the webkit page
+    page.close(Some(false)).await?;
+    // Chromium settings
+    let chromium = playwright.chromium();
+    let browser = chromium.launcher()
+        .headless(true)
         .downloads("C:\\Users\\jthom\\Desktop\\clips\\temp".as_ref())
         .launch().await?;
-    let context = browser
-        .context_builder()
+    let context = browser.context_builder()
         .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
         .accept_downloads(true)
         .build().await?;
-    let page = context
-        .new_page().await?;
 
-    // Navigate to the top English clips of the past 7 days
-    page.goto_builder("https://streamscharts.com/clips?language=en")
-        .goto().await?;
-    // Switch to table view
-    page.query_selector("div[class='flex items-center space-x-2']").await?.unwrap().query_selector_all("button").await?[1].click_builder().click().await?;
-
-    // Find all clips on the page
-    let clips_container = page.query_selector("div[class='x-cloak-hide']")
-        .await?.unwrap();
-    let mut clips = clips_container
-        .query_selector_all("tr[class='text-sm group']")
-        .await?;
-    let mut seen_clips = clips_container
-        .query_selector_all("tr[class='text-sm group']")
-        .await?; // Used to remove seen clips from the clips vector
-
-    let mut streamer_vec: Vec<String> = Vec::new();
-    let mut embed_vec: Vec<String> = Vec::new();
-    // While we don't yet have 100 embeds
-    while (streamer_vec.len() < 100) && (embed_vec.len() < 100) {
-        // If seen_clips and clips vector are different lengths, remove the clips that were already seen
-        println!("{} clips", clips.len());
-        println!("{} seen clips", seen_clips.len());
-
-        let remove_index = clips.len() - seen_clips.len();
-        if remove_index > 0 {
-            clips.drain(0..remove_index);
-        }
-        // Re-assign seen_clips with the clips now visible on the page
-        // Redundant only on the first iteration
-        seen_clips = clips_container
-            .query_selector_all("tr[class='text-sm group']")
-            .await?;
-        // Get the currently visible streamer names and embed links
-        let (temp_streamer, temp_embed) = get_clip_embeds(&page, &clips).await;
-        // Append the gathered info to the appropriate vectors
-        streamer_vec.append(&mut temp_streamer.clone());
-        embed_vec.append(&mut temp_embed.clone());
-        // After gathering the info, we should be at the bottom of the page, so click the "Show More" button
-       page.query_selector("div[class='relative flex justify-center mt-6']").await?.unwrap().query_selector("button").await?.unwrap().click_builder().click().await?;
-        tokio::time::sleep(std::time::Duration::from_millis(4000)).await;
-
-        // Re-assign clips vector with the newly visible clips
-        // This includes clips which have already been seen so they will be removed from the clips vector at the start of the next iteration
-        clips = clips_container
-            .query_selector_all("tr[class='text-sm group']")
-            .await?;
+    // Clear old clips from clips folder if any exist
+    let old_clips = fs::read_dir("C:\\Users\\jthom\\Desktop\\clips\\english clips").unwrap();
+    for clip in old_clips {
+        let _ = fs::remove_file(clip.unwrap().path());
     }
+    println!("Old clips removed");
+    // For each clip, navigate to clipsey.com and download it
+    for (i, url) in url_vec.iter().enumerate() {
+        // Open a new page
+        let page = context.new_page().await?;
+        // Navigate to clipsey.com
+        page.goto_builder("https://clipsey.com/").goto().await?;
+        let target = "https://www.twitch.tv".to_owned() + url.as_ref();
+        println!("{}", &target);
+        page.query_selector("input[class='clip-url-input']").await?
+            .expect("Can't find URL input :(")
+            .fill_builder(target.as_ref())
+            .fill().await?;
+        page.query_selector("button[class='get-download-link-button']").await?
+            .expect("Can't find submit button :(")
+            .click_builder()
+            .click().await?;
+        page.query_selector("a[class='download-clip-button button']").await?
+            .expect("Can't find download button :(")
+            .click_builder()
+            .click().await?;
 
+        // The downloaded clip has a random name, so we rename it to the clip ranking + the streamer's name
+        // Wait 5.5 seconds before trying to rename the file to prevent fuckery
+        tokio::time::sleep(tokio::time::Duration::from_millis(6000)).await;
+        let files = fs::read_dir("C:\\Users\\jthom\\Desktop\\clips\\temp").unwrap();
+        for file in files {
+            fs::remove_file(file).unwrap(); // In case any temp download files got stuck
+            fs::rename(file.unwrap().path(), format!("C:\\Users\\jthom\\Desktop\\clips\\english clips\\{}-{}.mp4", i+1, streamer_vec[i])).unwrap();
+        }
+        println!("Downloaded clip {} of {}\n", i+1, url_vec.len());
+        page.close(Some(false)).await?;
+    }
     // End of program
     Ok(())
 }
 
-async fn get_clip_embeds(page: &Page ,clips: &Vec<ElementHandle>) -> (Vec<String>, Vec<String>) {
+async fn clip_links(clip_vec: Vec<ElementHandle>) ->  (Vec<String>, Vec<String>) {
+    // Vector to keep track of clip URLs
+    let mut url_vec = Vec::new();
+    // Vector to keep track of clip streamers
     let mut streamer_vec = Vec::new();
-    let mut embed_vec = Vec::new();
-    // Iterate through all clips
-    for clip in clips {
-        let streamer = clip.query_selector("a").await.unwrap().unwrap().get_attribute("href").await.unwrap().unwrap().split("/").collect::<Vec<&str>>()[4].to_string(); // Streamer's name
-        println!("{}", streamer);
-        streamer_vec.push(streamer);
-        // Open the clip
-        let _ = clip.query_selector("div[class='t_s_p_c']").await
-            .unwrap().unwrap()
-            .click_builder()
-            .click().await;
-        // Wait a bit to prevent fuckery
-        tokio::time::sleep(std::time::Duration::from_millis(2000)).await;
-        // Clip player container
-        let big_clip = page.query_selector("div[class='relative w-full video-popup-position max-h-70h md:max-w-70w md:h-full']").await.unwrap().unwrap();
-        // Extract the embed link
-        let embed = big_clip.query_selector("iframe[class='ratio_container-inner']").await.unwrap().unwrap().get_attribute("src").await.unwrap().unwrap();
-        println!("{}", embed);
-        embed_vec.push(embed);
-        // Close the clip
-        let _ = big_clip.query_selector("svg[title='close']").await.unwrap().unwrap().click_builder().click().await;
+    // For each clip container, find the link to the clip, place it into the URL vector so that we can navigate to it later
+    // Also, extract the streamer's name from each clip URL
+    for (i, clip) in clip_vec.iter().enumerate() {
+        let url = clip.query_selector("a[data-a-target='preview-card-image-link']").await
+            .unwrap()
+            .expect("Can't find clip link :(")
+            .get_attribute("href").await
+            .expect("Can't find clip link :(");
+        println!("Creator of clip {}: {}", i+1, url.as_ref()
+            .unwrap()
+            .split("/")
+            .nth(1)
+            .unwrap());
+        streamer_vec.push(url.as_ref()
+            .unwrap().split("/")
+            .nth(1)
+            .unwrap()
+            .to_string());
+        url_vec.push(url.unwrap());
     }
-    (streamer_vec, embed_vec)
+    /*
+     * Instead of navigating to each clip and downloading directly, we will use a third party website to download the clips
+     * Therefore, we return the vectors so that we can use them later
+     * The streamer_vec Vector is technically not needed because the streamer name could be extracted from the URL later
+     * but this is easier for me so that's how it's going.
+     */
+    (streamer_vec, url_vec)
 }
